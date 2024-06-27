6dc463c19b26c2143a22eeedcd842653a96c4f6e
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-06-27 08:42:01 -0400

    ID-page.py

--- ID-page.py
+++ ID-page.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.markdown('<h1 style="color:black;">Vgg 19 Image classification model</h1>', unsafe_allow_html=True)
-st.markdown('<h2 style="color:gray;">The image classification model classifies image into following categories:</h2>', unsafe_allow_html=True)
-st.markdown('<h3 style="color:gray;"> street,  buildings, forest, sea, mountain, glacier</h3>', unsafe_allow_html=True)
+st.markdown('<h2 style="color:gray;">The image classification model classifies nematodes into following categories:</h2>', unsafe_allow_html=True)
+st.markdown('<h3 style="color:gray;"> Hoplolaimus,  Mesocriconema, Pratylenchus</h3>', unsafe_allow_html=True)
