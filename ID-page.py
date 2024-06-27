6883d23fe2acd31da77cedcf85172ba3cb6c67c0
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-06-27 14:22:17 -0400

    ID-page.py

--- ID-page.py
+++ ID-page.py
@@ -37,15 +37,4 @@ if upload is not None:
   c1.image(im)
   c1.write(img.shape)
 
-  ############################
-upload= st.file_uploader('Insert image for classification', type=['png','jpg'])
-c1, c2= st.columns(2)
-if upload is not None:
-  im= Image.open(upload)
-  img= np.asarray(im)
-  image= cv2.resize(img,(224, 224))
-  #img= preprocess_input(image)
-  img= np.expand_dims(img, 0)
-  c1.header('Input Image')
-  c1.image(im)
-  c1.write(img.shape)
+
