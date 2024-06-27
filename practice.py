925307ff0d87451d137721401e39c24df83dfdf7
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-06-27 13:56:14 -0400

    practice.py

--- practice.py
+++ practice.py
@@ -1,5 +1,7 @@
+#https://docs.streamlit.io/develop/api-reference
+
 import streamlit as st
 
 st.set_page_config(layout="wide", page_title="Nematode Image Classification")
 
-st.write("Identify nematodes")
+st.header("Nematode Image Classification")
