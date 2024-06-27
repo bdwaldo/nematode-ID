37a6957c062717bc015d377baaee5bb8981cdef0
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-06-27 08:56:13 -0400

    ID-page.py

--- ID-page.py
+++ ID-page.py
@@ -1,11 +1,14 @@
+#https://medium.com/geekculture/image-classifier-with-streamlit-887fc186f60
+
 import streamlit as st
 
-#webpage text
+#webpage text headers
 st.markdown('<h1 style="color:black;">Vgg 19 Image classification model</h1>', unsafe_allow_html=True)
 st.markdown('<h2 style="color:gray;">The image classification model classifies nematodes into following categories:</h2>', unsafe_allow_html=True)
 st.markdown('<h3 style="color:gray;"> Hoplolaimus,  Mesocriconema, Pratylenchus</h3>', unsafe_allow_html=True)
 
 #pre-processing image
+#adding text to web page to upload image
 upload= st.file_uploader('Insert image for classification', type=['png','jpg'])
 c1, c2= st.columns(2)
 if upload is not None:
@@ -17,3 +20,18 @@ if upload is not None:
   c1.header('Input Image')
   c1.image(im)
   c1.write(img.shape)
+
+  ############################
+#load weights of the trained model.
+  input_shape = (224, 224, 3)
+  optim_1 = Adam(learning_rate=0.0001)
+  n_classes=3
+  vgg_model = model(input_shape, n_classes, optim_1, fine_tune=2)
+  vgg_model.load_weights('/content/drive/MyDrive/vgg/tune_model19.weights.best.hdf5')
+
+  # prediction on model
+  vgg_preds = vgg_model.predict(img)
+  vgg_pred_classes = np.argmax(vgg_preds, axis=1)
+  c2.header('Output')
+  c2.subheader('Predicted class :')
+  c2.write(classes[vgg_pred_classes[0]] )
