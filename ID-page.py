a80adedc7c30fbbe9405d030783b7c2f68118619
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-06-27 08:46:34 -0400

    ID-page.py

--- ID-page.py
+++ ID-page.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
+#webpage text
 st.markdown('<h1 style="color:black;">Vgg 19 Image classification model</h1>', unsafe_allow_html=True)
 st.markdown('<h2 style="color:gray;">The image classification model classifies nematodes into following categories:</h2>', unsafe_allow_html=True)
 st.markdown('<h3 style="color:gray;"> Hoplolaimus,  Mesocriconema, Pratylenchus</h3>', unsafe_allow_html=True)
 
+#pre-processing image
 upload= st.file_uploader('Insert image for classification', type=['png','jpg'])
 c1, c2= st.columns(2)
 if upload is not None:
