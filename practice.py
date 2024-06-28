9a7790579a7a408e2298b88e39ab777903979453
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-06-28 15:26:35 -0400

    practice.py

--- practice.py
+++ practice.py
@@ -1,13 +1,39 @@
-#https://docs.streamlit.io/develop/api-reference
+#https://medium.com/geekculture/image-classifier-with-streamlit-887fc186f60
+
+#import streamlit as st
+#import numpy as np
+#import PIL 
+#import cv2
+#import tensorflow
+#from PIL import Image
 
 import streamlit as st
+from PIL import Image
+import numpy as np
+#import tensorflow as tf
+import matplotlib.pyplot as plt
+import cv2
 
-st.set_page_config(layout="wide", page_title="Nematode Image Classification")
+#webpage text headers
+#st.markdown('<h1 style="color:black;">Image classification model</h1>', unsafe_allow_html=False)
+#st.markdown('<h2 style="color:gray;">The image classification model classifies nematodes into following categories:</h2>', unsafe_allow_html=False)
+#st.markdown('<h3 style="color:gray;"> Hoplolaimus,  Mesocriconema, Pratylenchus</h3>', unsafe_allow_html=False)
 
-st.header("Nematode Image Classification")
-
+st.header("Plant-Parasitic Nematode Image Classification")
 st.markdown("Upload nematode image for identification")
+st.caption("Developed by UMD and USDA researchers")
 
-st.write("Developed by UMD and USDA researchers")
 
-
+#pre-processing image
+#adding text to web page to upload image
+upload= st.file_uploader('Select image for identification', type=['png','jpg'])
+c1, c2= st.columns(2)
+if upload is not None:
+  im= Image.open(upload)
+  img= np.asarray(im)
+  image= cv2.resize(img,(224, 224))
+  #img= preprocess_input(image)
+  img= np.expand_dims(img, 0)
+  c1.header('Input Image')
+  c1.image(im)
+  c1.write(img.shape)
