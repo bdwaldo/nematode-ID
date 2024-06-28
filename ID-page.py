db2ab7a0466ccafd72e360e6dc33f8d8df074608
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-06-28 17:52:46 -0400

    ID-page.py

--- ID-page.py
+++ ID-page.py
@@ -47,25 +47,12 @@ model.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 
 # predicting images
-#img = image.load_img('test1.jpg', target_size=(img_width, img_height))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
+#imgg = image.load_img('test1.jpg', target_size=(img_width, img_height))
+#x = image.img_to_array(img)
+#x = np.expand_dims(x, axis=0)
 
 images = np.vstack([x])
-classes = model.predict_classes(image)
-print(classes)
+classes = model.predict_classes(image, batch_size=10)
+print classes
 
-# predicting multiple images at once
-#img = image.load_img('test2.jpg', target_size=(img_width, img_height))
-#y = image.img_to_array(img)
-#y = np.expand_dims(y, axis=0)
-
-# pass the list of multiple images np.vstack()
-#images = np.vstack([x, y])
-#classes = model.predict_classes(images, batch_size=10)
-
-# print the classes, the images belong to
-#print classes
-#print classes[0]
-#print classes[0][0]
 
