c45fb6f36cbad8d67f5378b567b8c34462b9ba72
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-02 09:20:55 -0400

    ID-page.py

--- ID-page.py
+++ ID-page.py
@@ -42,9 +42,9 @@ model = tf.keras.models.load_model('nema_model.h5') #switch from load_model()
 if upload is not None:
   #img = tf.keras.utils.load_img(
   # im, target_size=(img_height, img_width))
-  image = keras.utils.load_img(image_path)
+  image = keras.utils.load_img(upload, target_size=(img_height, img_width))
   #input_arr = img_to_array(image)
-  img = image.load_img(upload, target_size=(img_height, img_width)) #https://github.com/streamlit/streamlit/issues/4101
+  #img = image.load_img(upload, target_size=(img_height, img_width)) #https://github.com/streamlit/streamlit/issues/4101
   img = img_to_array(img)
   img = np.expand_dims(img,axis = 0)
   st.write(model.predict(img, verbose=0)[0][0])
