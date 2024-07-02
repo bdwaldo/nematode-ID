fef83fd6e09aadda395fe9e49ae4b937d977c71a
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-02 13:26:16 -0400

    ID-page.py

--- ID-page.py
+++ ID-page.py
@@ -58,13 +58,13 @@ if upload is not None:
  
   predictions = model.predict(img_array)
   score = tf.nn.softmax(predictions[0])
-  c1.write(predictions)
-  c1.write(score)
-  st.write(class_names[np.argmax(score)], 100 * np.max(score))
+  #c1.write(predictions)
+  #c1.write(score)
+  #st.write(class_names[np.argmax(score)], 100 * np.max(score))
   
-  #st.write(
-    #"This image is most likely {} with a {:.2f} percent confidence."
-    #.format(class_names[np.argmax(score)], 100 * np.max(score)))
+  st.write(
+    "This image is most likely {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score)))
 
   #https://stackoverflow.com/questions/38971293/get-class-labels-from-keras-functional-model
   #y_classes = predictions.argmax(axis = -1)
