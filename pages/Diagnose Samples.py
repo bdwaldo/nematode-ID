cb64d7c9f695e24f8ebfd74d6d025c453f6c2066
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-03 15:25:50 -0400

    Delete pages directory

--- pages/Diagnose Samples.py
+++
@@ -1,9 +0,0 @@
-#Page One
-#https://www.geeksforgeeks.org/creating-multipage-applications-using-streamlit/
-
-import streamlit as st 
-  
-st.title("This is PageOne.")
-
-#display text in the greenbackground box
-st.sidebar.success("You are currently viewing Page One")
--- pages/Nematode Profiles.py
+++
@@ -1,6 +0,0 @@
-#page two 7-3-24 
-#https://www.geeksforgeeks.org/creating-multipage-applications-using-streamlit/
-
-import streamlit as st 
-  
-st.title("This is PageTwo.")
