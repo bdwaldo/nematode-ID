1fc8dc1ea82b610d7403f161af7f9363039033ce
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-03 15:15:41 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,12 +3,37 @@
 
 import streamlit as st 
   
-st.set_page_config(page_title = "This is a Multipage WebApp") 
-st.title("This is the Home Page.")
+#st.set_page_config(page_title = "This is a Multipage WebApp") 
+#st.title("This is the Home Page.")
 
 #create sidebar. Will have a background color green to indicate success
-st.sidebar.success("Select Any Page from here") 
+#st.sidebar.success("Select Any Page from here") 
 
 #create a directory in the same location as this file with the name, "pages".
 #The page names will showup in the didebar of this home page
 #pages are listed alphabetically by defalt
+
+
+dashboard = st.Page(
+    "reports/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True
+)
+bugs = st.Page("reports/bugs.py", title="Bug reports", icon=":material/bug_report:")
+alerts = st.Page(
+    "reports/alerts.py", title="System alerts", icon=":material/notification_important:"
+)
+
+search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+history = st.Page("tools/history.py", title="History", icon=":material/history:")
+
+if st.session_state.logged_in:
+    pg = st.navigation(
+        {
+            "Account": [logout_page],
+            "Reports": [dashboard, bugs, alerts],
+            "Tools": [search, history],
+        }
+    )
+else:
+    pg = st.navigation([login_page])
+
+pg.run()
