a677a799d324dfa3faa729b44e0f81ceb69a3602
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-03 15:41:43 -0400

    Update Home.py

--- multipage1/Home.py
+++ multipage1/Home.py
@@ -9,7 +9,10 @@ st.title("This is the Home Page.")
 #create sidebar. Will have a background color green to indicate success
 st.sidebar.success("Select Any Page from here") 
 
-st.Page(
+Diagnose = st.Page(
     "multipage1/Diagnose Samples/Diagnose Samples.py", title="Diagnose Samples", icon=":material/dashboard:", default=True
 )
-st.Page("Diagnose Samples/Single.py", title="Single", icon=":material/bug_report:")
+#st.Page("Diagnose Samples/Single.py", title="Single", icon=":material/bug_report:")
+
+pg = st.navigation(
+        {"Reports": [dashboard, bugs, alerts]})
