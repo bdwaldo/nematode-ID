d026456c716be33999932d99b0f1a10fac4b4efd
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-03 16:04:28 -0400

    Update Home.py

--- multipage1/Home.py
+++ multipage1/Home.py
@@ -3,11 +3,12 @@
 
 import streamlit as st 
   
-#st.set_page_config(page_title = "This is a Multipage WebApp") 
+st.set_page_config(page_title = "This is a Multipage WebApp") 
 st.title("This is the Home Page.")
 
 #create sidebar. Will have a background color green to indicate success
 st.sidebar.success("Select Any Page from here") 
+ 
 
 
 #########################
