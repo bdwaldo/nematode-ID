adb4a9422a3fe2fd9a94fa3614e51d2539af758e
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 08:37:42 -0400

    Update home.py

--- mulitipage2/home.py
+++ mulitipage2/home.py
@@ -1,29 +1,25 @@
+#https://docs.streamlit.io/get-started/tutorials/create-a-multipage-app
 
 import streamlit as st
 
 st.set_page_config(
     page_title="Hello",
-    page_icon="👋",
+    #page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome to Nematode ID!")
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Select a Page.")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
+    Nematode ID is a source for identifying plant-parasitic nematodes from images.
+    **Select a page from the sidebar** to get started.
     ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
+    - Check out [streamlit.io](https://streamlit.io) #text to display in [] andlink in ()
+
     ### See more complex demos
     - Use a neural net to [analyze the Udacity Self-driving Car Image
         Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
