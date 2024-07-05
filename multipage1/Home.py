326dd896ef9dec464235e25869873bb3a915f4b2
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 09:01:24 -0400

    Update Home.py

--- multipage1/Home.py
+++ multipage1/Home.py
@@ -15,4 +15,24 @@ st.sidebar.success("Select Any Page from here")
 #pg = st.navigation([st.Page("pages/ID Now.py"), st.Page("pages/Nematode Profiles.py")])
 #pg.run()
 
+#####################
+diagnose = st.Page(
+    "Diagnose/Diagnose Samples.py", title="Diagnose", icon=":material/dashboard:", default=True
+)
+single = st.Page("Diagnose/Single.py", title="Single", icon=":material/bug_report:")
+batch = st.Page(
+    "Diagnose/Batch.py", title="Batch", icon=":material/notification_important:"
+)
+id_now = st.Page("pages/ID now.py", title = "ID Now", default = True)
+
+pg = st.navigation(
+        {
+            "Diagnose": [logout_page],
+            "Diagnose": [dashboard, bugs, alerts],
+            "ID now": [id_now]
+        }
+    )
+
+pg.run()
+
 
