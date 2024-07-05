95097a5a8d0060b2f201192eecd77651a2be62a3
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 14:16:26 -0400

    Update generate_keys.py

--- multipage1/diagnose/generate_keys.py
+++ multipage1/diagnose/generate_keys.py
@@ -13,3 +13,4 @@ hashed_passwords = stauth.Hasher(passwords).generate()
 file_path = Path(_file_).parent / "hashed_pw.pkl"
 with file_path.open("wb") as file:
   pickle.dump(hashed_passwords, file)
+ 
