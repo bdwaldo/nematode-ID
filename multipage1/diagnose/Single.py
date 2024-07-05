ef466ad3792b1b364114856dd651bbc0471505cf
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 12:07:30 -0400

    Update Single.py

--- multipage1/diagnose/Single.py
+++ multipage1/diagnose/Single.py
@@ -3,7 +3,7 @@ import streamlit as st
 form = st.form("my_form")
 st.write("Sample Information")
 st.text_input("Sample ID")
-st.selectbox('Sample Type', ["soil", "roots", "leaves"])
+st.selectbox('Sample Type', ["","soil", "roots", "leaves"])
 
 # Now add a submit button to the form:
 form.form_submit_button("Submit")
