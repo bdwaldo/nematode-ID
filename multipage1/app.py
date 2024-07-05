b6349af24233948a830b83ffa6e0555feb5f6eeb
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 09:37:06 -0400

    Update app.py

--- multipage1/app.py
+++ multipage1/app.py
@@ -1,1 +1,30 @@
+#https://docs.streamlit.io/develop/concepts/multipage-apps/page-and-navigation
 
+home = st.Page(
+    "multipage1/home.py", title="Home", icon=":material/dashboard:", default=True
+)
+id_Now = st.Page("multipage1/ID Now.py", title="ID Now", icon=":material/bug_report:")
+diagnose = st.Page(
+    "diagnose/Diagnose Samples.py", title="Diagnose Samples", icon=":material/notification_important:"
+)
+
+tutorial = st.Page(
+    "multipage1/help/Tutorial.py", title="Tutorial", icon=":material/dashboard:", default=True
+)
+
+help = st.Page(
+    "multipage1/help/Help.py", title="Tutorial", icon=":material/dashboard:", default=True
+)
+
+
+
+pg = st.navigation(
+  {
+    "Home": [home],
+    "ID Now": [id_now],
+    "Diagnose": [search, history],
+    "Help":[tutorial, help]
+  }
+)
+
+pg.run()
