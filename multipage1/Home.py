30bb3fd91f02ea8769e22db1300eef38ff10c005
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 10:24:35 -0400

    Update Home.py

--- multipage1/Home.py
+++ multipage1/Home.py
@@ -1,1 +1,24 @@
+#https://docs.streamlit.io/get-started/tutorials/create-a-multipage-app
 
+import streamlit as st
+
+#st.set_page_config(
+#    page_title="Welcome"
+   
+)
+
+st.write("# Welcome to Nematode ID!")
+
+st.sidebar.success("Select a Page.")
+
+st.markdown(
+    """
+    Nematode ID is a source for identifying plant-parasitic nematodes from images.
+    **Select a page from the sidebar** to get started.
+    ### Want to learn more?
+    - Check out the [Tutorial](https://streamlit.io)
+
+    ### Basic Information
+    - See the [Help](https://github.com/streamlit/demo-self-driving) page
+"""
+)
