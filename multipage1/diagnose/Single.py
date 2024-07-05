ba021d8561a4ec9058b3cee3a053b657acd6eaaf
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 12:02:27 -0400

    Update Single.py

--- multipage1/diagnose/Single.py
+++ multipage1/diagnose/Single.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 form = st.form("my_form")
 st.write("Sample Information")
+st.text_input("Sample Number")
 
 # Now add a submit button to the form:
 form.form_submit_button("Submit")
