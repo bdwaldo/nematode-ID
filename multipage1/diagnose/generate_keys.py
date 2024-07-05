fe89410d024583a045df4e0a7204a137ab717bfa
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 15:51:28 -0400

    Update generate_keys.py

--- multipage1/diagnose/generate_keys.py
+++ multipage1/diagnose/generate_keys.py
@@ -1,17 +1,18 @@
 #https://www.youtube.com/watch?v=JoFGrSRj4X4
-#import pickle
-#from pathlib import Path
+import pickle
+from pathlib import Path
+from streamlit_authenticator.utilities import hasher
 
-#import streamlit_authenticator as stauth
+import streamlit_authenticator as stauth
 
-#names = ["Benjamin"]
-#usernames = ["nema-guy"]
-#passwords = ["XXX"]
+names = ["Benjamin"]
+usernames = ["nema-guy"]
+passwords = ["XXX"]
 
-#hashed_passwords = stauth.Hasher(passwords).generate()
+hashed_passwords = stauth.Hasher(passwords).generate()
 
-#create pickle file and save information
-#file_path = Path(_file_).parent / "hashed_pw.pkl"
-#with file_path.open("wb") as file:
-#  pickle.dump(hashed_passwords, file)
+create pickle file and save information
+file_path = Path(_file_).parent / "hashed_pw.pkl"
+with file_path.open("wb") as file:
+  pickle.dump(hashed_passwords, file)
  
