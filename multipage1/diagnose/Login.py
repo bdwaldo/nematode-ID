98402fc3dfa003168975c4ecaca13add1443b2b1
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 15:20:55 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -1,25 +1,26 @@
-import pickle
-from pathlib import Path
+#https://www.youtube.com/watch?v=JoFGrSRj4X4
+#import pickle
+#from pathlib import Path
 
-import streamlit_authenticator as stauth
+#import streamlit_authenticator as stauth
 
-names = ["Benjamin"]
-usernames = ["nema-guy"]
+#names = ["Benjamin"]
+#usernames = ["nema-guy"]
 
 #load hashed passwords
-file_path = Path(_file_).Parent / "hashed_pw.pkl"
-with file_path.open("rb") as file: #open with root binary mode
-    hased_passwords = pickle.load(file) #load pickle file
+#file_path = Path(_file_).Parent / "hashed_pw.pkl"
+#with file_path.open("rb") as file: #open with root binary mode
+#    hased_passwords = pickle.load(file) #load pickle file
 
 #create authenticator object
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords, 
-                                    "nema-ID", "4fet", cookie_expiry_days=1) #cookie
+#authenticator = stauth.Authenticate(names, usernames, hashed_passwords, 
+#                                    "nema-ID", "4fet", cookie_expiry_days=1) #cookie
 
-name, authentication_status, username = authenticator.login("Login", "main")
+#name, authentication_status, username = authenticator.login("Login", "main")
 
 #authentication status
-if authentication_status == False: 
-    st.error("Username/password is incorrent")
+#if authentication_status == False: 
+#    st.error("Username/password is incorrent")
 
 if authentification_status == None:
     st.warning("Please enter your uername and password")
