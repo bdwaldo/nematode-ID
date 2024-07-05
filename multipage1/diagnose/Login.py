1245097c8554ef40b95963567f54177e09eab067
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 15:02:34 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -4,13 +4,25 @@ from pathlib import Path
 import streamlit_authenticator as stauth
 
 names = ["Benjamin"]
-usernames = ["nema"]
-passwords = [""]
+usernames = ["nema-guy"]
 
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-#create pickle file and save information
+#load hashed passwords
 file_path = Path(_file_).Parent / "hashed_pw.pkl"
-with file_path.open("nb") as file:
-  pickle.dump(hashed_passwords, file)
- 
+with file_path.open("rb") as file: #open with root binary mode
+    hashed_passwords = pickle.load(file) #load pickle file
+
+#create authenticator object
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, 
+                                    "nema-ID", "4fet", cookie_expiry_days=1) #cookie
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+#authentication status
+if authentication_status == False: 
+    st.error("Username/password is incorrent")
+
+if authentification_status == None:
+    st.warning("Please enter your uername and password")
+
+authenticator.logout("Logout", "sidebar")
+st.sidebar.title(f"Welcome {name}")
