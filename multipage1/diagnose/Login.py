16bda6d2e6c5edd4832ee7219673abc8f254c1d9
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 14:07:58 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -7,7 +7,7 @@ names = ["Benjamin"]
 usernames = ["nema-guy"]
 
 #load hashed passwords
-file_path = Path(_file_).Parent / "hashed_pw.pkl
+file_path = Path(_file_).Parent / "hashed_pw.pkl"
 with file_path.open("rb") as file: #open with root binary mode
     hased_passwords = pickle.load(file) #load pickle file
 
