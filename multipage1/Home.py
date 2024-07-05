6280ef4698f7ae9ea3f2b5f09178144e47e139be
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 08:14:10 -0400

    Update Home.py

--- multipage1/Home.py
+++ multipage1/Home.py
@@ -2,9 +2,9 @@
 #https://www.geeksforgeeks.org/creating-multipage-applications-using-streamlit/
 
 import streamlit as st 
-  
-st.set_page_config(page_title = "This is a Multipage WebApp") 
-st.title("This is the Home Page.")
+
+st.set_page_config(page_title = "Nematode ID") 
+st.title("Identify Now.")
 
 #create sidebar. Will have a background color green to indicate success
 st.sidebar.success("Select Any Page from here") 
