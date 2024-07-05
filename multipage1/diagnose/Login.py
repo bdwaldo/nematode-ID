3fa9c6547d1e80af1970ea5ba6177edbfce34432
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-05 15:39:39 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -1,29 +1,73 @@
-#https://www.youtube.com/watch?v=JoFGrSRj4X4
-#import pickle
-#from pathlib import Path
+import streamlit_authenticator as stauth
 
-#import streamlit_authenticator as stauth
+hashed_passwords = stauth.Hasher(['XXX', 'XXX']).generate()
 
-#names = ["Benjamin"]
-#usernames = ["nema-guy"]
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-#load hashed passwords
-#file_path = Path(_file_).Parent / "hashed_pw.pkl"
-#with file_path.open("rb") as file: #open with root binary mode
-#    hased_passwords = pickle.load(file) #load pickle file
+name, authentication_status, username = authenticator.login('Login', 'main')
 
-#create authenticator object
-#authenticator = stauth.Authenticate(names, usernames, hashed_passwords, 
-#                                    "nema-ID", "4fet", cookie_expiry_days=1) #cookie
+if authentication_status:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
 
-#name, authentication_status, username = authenticator.login("Login", "main")
 
-#authentication status
-#if authentication_status == False: 
-#    st.error("Username/password is incorrent")
+if authentication_status:
+    try:
+        if authenticator.reset_password(username, 'Reset password'):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
 
-if authentification_status == None:
-    st.warning("Please enter your uername and password")
 
-authenticator.logout("Logout", "sidebar")
-st.sidebar.title(f"Welcome {name}")
+try:
+    if authenticator.register_user('Register user', preauthorization=False):
+        st.success('User registered successfully')
+except Exception as e:
+    st.error(e)
+
+
+try:
+    username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
+    if username_forgot_pw:
+        st.success('New password sent securely')
+        # Random password to be transferred to the user securely
+    else:
+        st.error('Username not found')
+except Exception as e:
+    st.error(e)
+    
+
+try:
+    username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
+    if username_forgot_username:
+        st.success('Username sent securely')
+        # Username to be transferred to the user securely
+    else:
+        st.error('Email not found')
+except Exception as e:
+    st.error(e)
+
+
+if authentication_status:
+    try:
+        if authenticator.update_user_details(username, 'Update user details'):
+            st.success('Entries updated successfully')
+    except Exception as e:
+        st.error(e)
+
+
+with open('config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
