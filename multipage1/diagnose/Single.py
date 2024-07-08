1402a73cef5002891c5f063a0dce3bfe14fc6526
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-08 13:36:18 -0400

    Update Single.py

--- multipage1/diagnose/Single.py
+++ multipage1/diagnose/Single.py
@@ -3,13 +3,18 @@ import streamlit as st
 
 with st.form("my_form"):
   st.write("Sample Information")
-  st.text_input("Sample ID")
-  st.text_input("State sample collected")
-  st.text_input("Location identifier (field, green...etc)") 
-  st.selectbox('Sample Type', ["","soil", "roots", "leaves"])
-  st.text_input("Crop")
-  st.text_input("Variety")
-  st.selectbox('Symptoms', ["","none", "galling", "stunted roots", "wilting", "chlorosis", "other"])
+  sample_id = st.text_input("Sample ID")
+  sample_collected = st.text_input("State sample collected")
+  location = st.text_input("Location identifier (field, green...etc)") 
+  sample_type = st.selectbox('Sample Type', ["","soil", "roots", "leaves"])
+  crop = st.text_input("Crop")
+  variety = st.text_input("Variety")
+  symptoms = st.selectbox('Symptoms', ["","none", "galling", "stunted roots", "wilting", "chlorosis", "other"])
   submitted = st.form_submit_button('Submit')
 
-
+d = {‘Book Name(s)’: [newBookName],
+‘Book Author(s)’: [newBookAuthor],
+‘Feedback’: [newFeedback],
+‘Name’: [emailName],
+‘Email’: [emailAddress]}
+df = pd.DataFrame(data=d)
