3e679b53421ec2ff2d50d16eb66c3312f20ff5ba
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-08 13:50:21 -0400

    Update Single.py

--- multipage1/diagnose/Single.py
+++ multipage1/diagnose/Single.py
@@ -3,8 +3,12 @@ import streamlit as st
 
 with st.form("my_form"):
   st.write("Sample Information")
+  sample_submitter = st.text_input("Sample submitter")
+  phone = st.text_input("Submitter phone number")
+  email = st.text_input("Submitter email address")
   sample_id = st.text_input("Sample ID")
-  sample_collected = st.text_input("State sample collected")
+  date = st.date_input("Date sample collected")
+  state = st.text_input("State sample collected")
   location = st.text_input("Location identifier (field, green...etc)") 
   sample_type = st.selectbox('Sample Type', ["","soil", "roots", "leaves"])
   crop = st.text_input("Crop")
@@ -12,9 +16,17 @@ with st.form("my_form"):
   symptoms = st.selectbox('Symptoms', ["","none", "galling", "stunted roots", "wilting", "chlorosis", "other"])
   submitted = st.form_submit_button('Submit')
 
-d = {‘Book Name(s)’: [newBookName],
-‘Book Author(s)’: [newBookAuthor],
-‘Feedback’: [newFeedback],
-‘Name’: [emailName],
-‘Email’: [emailAddress]}
-df = pd.DataFrame(data=d)
+df = {'Sample submitter': [sample_submitter],
+      'Sample ID': [sample_id],
+      'Submitter Phone': [phone],
+      'Email': [email]
+      'Date collected': [date],
+      'State Sample Collected': [state],
+      'Location': [location],
+      'Sample Type': [sample_type],
+      'Crop': [crop],
+      'Variety': [variety],
+      'Symptoms': [symptoms],
+           }
+df = df.append(pd.DataFrame(data=df))
+print(df)
