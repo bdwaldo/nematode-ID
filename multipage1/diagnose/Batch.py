e0cc4413c11fc4d5b2f5f0d98c6fd8b30ee65626
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-09 14:09:50 -0400

    Update Batch.py

--- multipage1/diagnose/Batch.py
+++ multipage1/diagnose/Batch.py
@@ -3,6 +3,14 @@ import csv
 import pandas as pd
 
 
+with st.form("my_form"):
+  st.write("Sample Information")
+  submitter = st.text_input("Submitter name")
+  phone = st.text_input("Submitter phone number")
+  email = st.text_input("Submitter email address")
+  date = st.date_input("Date sample collected", format="MM.DD.YYYY")
+
+
 
 
 edit_df = pd.DataFrame(
@@ -18,12 +26,18 @@ edit_df = pd.DataFrame(
 )
 
 
-#fields = [sample_submitter, sample_id, phone, 
-#          email, date, state, location,
-#          sample_type, crop, variety, symptoms]
-
-
-
+df = {'Sample submitter': [submitter],
+      'Sample ID': [sample_id],
+      'Submitter Phone': [phone],
+      'Email': [email],
+      'Date collected': [date],
+      'State Sample Collected': [state],
+      'Location': [location],
+      'Sample Type': [sample_type],
+      'Crop': [crop],
+      'Variety': [variety],
+      'Symptoms': [symptoms],
+           }
 
 #convert data into pandas data frame
 #df = pd.DataFrame(data=df)
