de32712e22e22cffe653db10a7fdb9833fbfebbb
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-09 15:35:56 -0400

    Update Batch.py

--- multipage1/diagnose/Batch.py
+++ multipage1/diagnose/Batch.py
@@ -15,15 +15,17 @@ edit_df = pd.DataFrame(
   ]
 )
 
+#Generate form with text boxes and a text editor box for multiple entries
 with st.form("my_form"):
-  st.write("Sample Information")
+  st.write("Sample Submission Information")
   submitter = st.text_input("Submitter name")
   phone = st.text_input("Submitter phone number")
   email = st.text_input("Submitter email address")
   date = st.date_input("Date sample collected", format="MM.DD.YYYY")
   ed = st.data_editor(edit_df, num_rows = "dynamic")
-  submitted = st.form_submit_button('Submit')
+  submitted = st.form_submit_button('Submit Form')
 
+#data frame of text entry boxes
 df = {'Sample submitter': [submitter],
       'Submitter Phone': [phone],
       'Email': [email],
@@ -38,6 +40,7 @@ df3 = pd.concat([df, df2])
 
 
 ##############################
+#generate a unique ID based on webpage visit number
 #AI generated code
 # Initialize a list to store form submissions
 @st.cache(allow_output_mutation=True)
@@ -60,17 +63,18 @@ st.write(name)
 
 
 ##########################################
-#positioning buttons in columns
+#positioning summary and download buttons in columns
 
 col1, col2 = st.columns(2)
 
-#See summary of sample info
+#Button to see summary of sample submission info
 with col1:
   if st.button('Summary'):
     st.write(df3)
 
 
 #################################################
+#button to download csv of sample submission info
 #convert data frame to csv and download
 #https://docs.streamlit.io/knowledge-base/using-streamlit/how-download-pandas-dataframe-csv
 with col2:
