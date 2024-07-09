cd1d8be998c3f87e2fe0523396e361a92a7e22a7
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-09 14:41:42 -0400

    Update Batch.py

--- multipage1/diagnose/Batch.py
+++ multipage1/diagnose/Batch.py
@@ -1,4 +1,4 @@
-import streamlit as st
+dimport streamlit as st
 import csv
 import pandas as pd
 
@@ -21,17 +21,19 @@ with st.form("my_form"):
   phone = st.text_input("Submitter phone number")
   email = st.text_input("Submitter email address")
   date = st.date_input("Date sample collected", format="MM.DD.YYYY")
-  st.data_editor(edit_df, num_rows = "dynamic")
+  ed = st.data_editor(edit_df, num_rows = "dynamic")
   submitted = st.form_submit_button('Submit')
 
 df = {'Sample submitter': [submitter],
       'Submitter Phone': [phone],
       'Email': [email],
-      'Date collected': [date]
+      'Date collected': [date],
+      'Edited': [ed]
      }
 
 #convert data into pandas data frame
 df = pd.DataFrame(data=df)
+
 
 ##############################
 #AI generated code
