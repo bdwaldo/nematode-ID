f6d913b50c0ff3920077c116fa78d7d56037b318
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-09 17:03:33 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -23,14 +23,7 @@ authenticator.login()
 #name, authentication_status, username = authenticator.login('Login', 'main')
 
 #Authenticating users
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
-    st.title('Some content')
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+
 
 
 if st.session_state["authentication_status"]:
