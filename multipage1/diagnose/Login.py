e75d08320b4b5c1cd66142c5e72de5300d0109d9
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-09 16:24:25 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -36,24 +36,24 @@ elif st.session_state["authentication_status"] is None:
 
 
 #Creating a reset password widget
-if st.session_state["authentication_status"]:
-    try:
-        if authenticator.reset_password(st.session_state["username"]):
-            st.success('Password modified successfully')
-    except Exception as e:
-        st.error(e)
+#if st.session_state["authentication_status"]:
+#    try:
+#        if authenticator.reset_password(st.session_state["username"]):
+#            st.success('Password modified successfully')
+#    except Exception as e:
+#        st.error(e)
 
 
 #Creating a new user registration widget
-try:
-    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
-    if username_of_forgotten_password:
-        st.success('New password to be sent securely')
-        # The developer should securely transfer the new password to the user.
-    elif username_of_forgotten_password == False:
-        st.error('Username not found')
-except Exception as e:
-    st.error(e)
+#try:
+#    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
+#    if username_of_forgotten_password:
+#        st.success('New password to be sent securely')
+#        # The developer should securely transfer the new password to the user.
+#    elif username_of_forgotten_password == False:
+#        st.error('Username not found')
+#except Exception as e:
+#    st.error(e)
 
 
 #Creating a forgot password widget
