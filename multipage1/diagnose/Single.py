4166234cecf37c48b1038a7ff153cd576133bcae
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-09 08:02:57 -0400

    Update Single.py

--- multipage1/diagnose/Single.py
+++ multipage1/diagnose/Single.py
@@ -44,7 +44,7 @@ st.write(df)
 
 #look up "convert pandas data frame to csv streamlit"
 #convert df to binary
-csv_data = df.to_csv(df, index = False).encode('utf-8')
+csv_data = df.to_csv(df, index = False)
 #csv_data=convert_df_to_csv(csv_data)
 
 
