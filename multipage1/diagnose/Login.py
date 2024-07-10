863def0347babe806d6f7ee7ad71980180355276
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-10 12:34:56 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -9,7 +9,7 @@ with st.form(key='signup', clear_on_submit = True):
   st.subheader('Sign Up')
   st.form_submit_button('Sign Up')
   email = st.text_input('Email', placeholder='Enter your email')
-  Name = st.text_input('Name', placeholder='Enter your name')
+  name = st.text_input('Name', placeholder='Enter your name')
   password1 = st.text_input('Password', placeholder='Enter your password')
   password2 = st.text_input('Confirm Password', placeholder='Confirm password')
   
