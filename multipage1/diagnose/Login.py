1e57ce068c509404b066e0adf1850f120724ef09
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-10 10:20:39 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -4,6 +4,6 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 
-def sign_up():
-  with st.form(key='signup', slear_on_submit = True):
-    at.subheader('Sign Up')
+
+with st.form(key='signup', clear_on_submit = True):
+  st.subheader('Sign Up')
