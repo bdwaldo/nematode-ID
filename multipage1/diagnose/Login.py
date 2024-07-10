74227175093dae30c3b4e7df2ddd960c94926748
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-10 12:37:19 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -6,9 +6,10 @@ import streamlit_authenticator as stauth
 
 
 with st.form(key='signup', clear_on_submit = True):
-  st.subheader('Sign Up')
+  st.subheader('Create account')
   email = st.text_input('Email', placeholder='Enter your email')
   name = st.text_input('Name', placeholder='Enter your name')
+  organization = st.text_input('Organization', placeholder='Enter your organization')
   password1 = st.text_input('Password', placeholder='Enter your password')
   password2 = st.text_input('Confirm Password', placeholder='Confirm password')
   st.form_submit_button('Sign Up')
