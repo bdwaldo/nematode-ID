f292a9d6f724ffb22a35d94d4ddb34f34eca6b42
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-10 15:44:29 -0400

    Update dependencies.py

--- multipage1/diagnose/dependencies.py
+++ multipage1/diagnose/dependencies.py
@@ -1,38 +1,100 @@
 #login page
-#https://pypi.org/project/streamlit-authenticator/
+#https://github.com/Frost-Codes/Streamlit-Authentication/blob/main/dependancies.py
   
 import streamlit as st
 import streamlit_authenticator as stauth
 
-def sign_up():
-  with st.form(key='signup', clear_on_submit = True):
-    st.subheader('Create account')
-    email = st.text_input('Email', placeholder='Enter your email')
-    name = st.text_input('Name', placeholder='Enter your name')
-    organization = st.text_input('Organization', placeholder='Enter your organization')
-    password1 = st.text_input('Password', placeholder='Enter your password')
-    password2 = st.text_input('Confirm Password', placeholder='Confirm password')
-  
-    if email:
-      if validate_email(email):
-        if email not in get_user_emails():
-          if validate_username(username):
-            if username not in get_usernames():
-              if password1 == password2:
-                hashed_password = stauth.Hasher([password2]).generate #add user to database
-                insert_user(email, username, hashed_password[0])
-                st.success('Account created successfully')
-              else:
-                st.warning('Passwords do not match')
-            else:
-              st.warning('Username Already Exists')
-          else:
-            st.warning('Invalid Username')
-        else:
-          st.warning('Invalid Email')
-    st.form_submit_button('Sign Up')  
+###########################
+#Need to connect database##
+###########################
 
-sign_up()
+#DETA_KEY = 'YOUR_DETA_KEY'
+
+#deta = Deta(DETA_KEY)
+
+#db = deta.Base('YOUR_DETA_BASE')
+
+
+def insert_user(email, username, password):
+    """
+    Inserts Users into the DB
+    :param email:
+    :param username:
+    :param password:
+    :return User Upon successful Creation:
+    """
+    date_joined = str(datetime.datetime.now())
+
+    return db.put({'key': email, 'username': username, 'password': password, 'date_joined': date_joined})
+
+
+def fetch_users():
+    """
+    Fetch Users
+    :return Dictionary of Users:
+    """
+    users = db.fetch()
+    return users.items
+
+
+def get_user_emails():
+    """
+    Fetch User Emails
+    :return List of user emails:
+    """
+    users = db.fetch()
+    emails = []
+    for user in users.items:
+        emails.append(user['key'])
+    return emails
+
+
+def get_usernames():
+    """
+    Fetch Usernames
+    :return List of user usernames:
+    """
+    users = db.fetch()
+    usernames = []
+    for user in users.items:
+        usernames.append(user['key'])
+    return usernames
+
+
+def validate_email(email):
+    """
+    Check Email Validity
+    :param email:
+    :return True if email is valid else False:
+    """
+    pattern = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$" #tesQQ12@gmail.com
+
+    if re.match(pattern, email):
+        return True
+    return False
+
+
+def validate_username(username):
+    """
+    Checks Validity of userName
+    :param username:
+    :return True if username is valid else False:
+    """
+
+    pattern = "^[a-zA-Z0-9]*$"
+    if re.match(pattern, username):
+        return True
+    return False
+
+
+
+
+
+
+
+
+
+#paste code from other python file here
 
                    
                    
