d1a13e66a79a8540edd10a9953ec7ab1d8614591
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 13:40:16 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -32,43 +32,22 @@ elif st.session_state["authentication_status"] is False:
 elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
 
-#reset password widget
-if st.session_state["authentication_status"]:
-    try:
-        if authenticator.reset_password(st.session_state["username"]):
-            st.success('Password modified successfully')
-    except Exception as e:
-        st.error(e)
-
-#create new user
-try:
-    email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
-    if email_of_registered_user:
-        st.success('User registered successfully')
-except Exception as e:
-    st.error(e)
-
-    
-#Forgot pasword widget
-try:
-    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
-    if username_of_forgotten_password:
-        st.success('New password to be sent securely')
-        # The developer should securely transfer the new password to the user.
-    elif username_of_forgotten_password == False:
-        st.error('Username not found')
-except Exception as e:
-    st.error(e)
-
 
 
 #update user information
-if st.session_state["authentication_status"]:
-    try:
-        if authenticator.update_user_details(st.session_state["username"]):
-            st.success('Entries updated successfully')
-    except Exception as e:
-        st.error(e)
+usernames = config['cookie']['name']
+
+
+with st.form("edit_profile"):
+  st.write("Edit Profile")
+  new_name = st.text_input("New name")
+  submitted = st.form_submit_button('Submit Form')
+  if (config['cookie']['name'] == new_name):
+      st.warning('Name already in use.')
+  else:
+      config['cookie']['name'] = new_name
+
+
 
 
 #update config file
