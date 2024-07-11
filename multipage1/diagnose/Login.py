e3ceef5498f3406e090aba767b592ef2fab04020
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 08:48:34 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -9,7 +9,15 @@ st.caption('''I have created accounts manually for this example.
 I would need to connect the signup page to a database in order to 
 securely store and update user information.''') 
 
+def signup(): {
+  st.form(key='login', clear_on_submit = True) 
+  st.subheader('Login')
+  username = st.text_input('Username', placeholder='Enter your username')
+  name = st.text_input('Password', placeholder='Enter your password')
 
+}
+
+  
 
 
 #print username list (un)
