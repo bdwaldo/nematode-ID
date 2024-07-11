fe973b0003e2d8cc613a30635ed29b8ff4a55d66
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 08:40:28 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -5,9 +5,9 @@ st.header("Login to account")
 st.markdown('''This is a demo for a login page. I envision using accounts to store and track
 sample information for diagnostics.''')
 
-st.caption("I have created accounts manually for this example. 
+st.caption('''I have created accounts manually for this example. 
 I would need to connect the "signup" page to a database in order to 
-securely store and update user information.) 
+securely store and update user information.''') 
 
 #print username list (un)
 for x in range(len(st.secrets.un)):
