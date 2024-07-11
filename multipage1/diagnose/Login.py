9c382abaeca8a1f1fd7d672bc5170e930cfbcc42
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 08:35:22 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import os
 
+st.header("Login to account")
+st.markdown("This is a demo for a login page. I envision using accounts to store and track
+sample information for diagnostics.")
 
+st.caption("I have created accounts manually for this example. 
+I would need to connect the "signup" page to a database in order to 
+securely store and update user information.) 
 
 #print username list (un)
 for x in range(len(st.secrets.un)):
