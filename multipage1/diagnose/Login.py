224a93a9adc3330ec74e8146e091840c2a1d3cef
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 10:48:19 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -1,8 +1,12 @@
+#streamlit authenticator login
+#https://github.com/mkhorasani/Streamlit-Authenticator/tree/main?tab=readme-ov-file#authenticatelogin
+
 import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 
 # Load your configuration file (replace '../config.yaml' with your actual path)
+#make sure yaml file extension is correct. won't work if created in jupytr notebook
 with open('multipage1/diagnose/config.yaml') as file:
     config = yaml.load(file, Loader=yaml.SafeLoader)
 
@@ -15,7 +19,8 @@ authenticator = stauth.Authenticate(
     config['pre-authorized']
 )
 
-#https://github.com/mkhorasani/Streamlit-Authenticator/tree/main?tab=readme-ov-file#authenticatelogin
+
+#this function is the up to date version
 authenticator.login()
 
 if st.session_state["authentication_status"]:
@@ -26,3 +31,48 @@ elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
+
+#reset password widget
+if st.session_state["authentication_status"]:
+    try:
+        if authenticator.reset_password(st.session_state["username"]):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
+
+
+#new user widget
+try:
+    email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
+    if email_of_registered_user:
+        st.success('User registered successfully')
+except Exception as e:
+    st.error(e)
+
+    
+#Forgot pasword widget
+try:
+    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
+    if username_of_forgotten_password:
+        st.success('New password to be sent securely')
+        # The developer should securely transfer the new password to the user.
+    elif username_of_forgotten_password == False:
+        st.error('Username not found')
+except Exception as e:
+    st.error(e)
+
+
+
+#forgot username widget
+if st.session_state["authentication_status"]:
+    try:
+        if authenticator.update_user_details(st.session_state["username"]):
+            st.success('Entries updated successfully')
+    except Exception as e:
+        st.error(e)
+
+
+
+#update config file
+with open('multipage1/diagnose/config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
