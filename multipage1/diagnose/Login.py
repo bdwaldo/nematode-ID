550bd99a688e8e25b7392dcd2eca84872682aa41
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 08:39:36 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -2,8 +2,8 @@ import streamlit as st
 import os
 
 st.header("Login to account")
-st.markdown("This is a demo for a login page. I envision using accounts to store and track
-sample information for diagnostics.")
+st.markdown('''This is a demo for a login page. I envision using accounts to store and track
+sample information for diagnostics.''')
 
 st.caption("I have created accounts manually for this example. 
 I would need to connect the "signup" page to a database in order to 
