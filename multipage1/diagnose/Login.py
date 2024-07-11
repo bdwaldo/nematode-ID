9ac97fa3cbd94f95a87049a835afbf1713ac9764
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 08:23:52 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import os
 
-st.write(st.secrets.un[])
+for x in range(len(st.secrets.un)):
+  st.write(st.secrets.un[x])
 
 st.write(st.secrets.b["username"])
 st.write(st.secrets.b["name"])
