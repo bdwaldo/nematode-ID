caa74fa41b810527322fd3b529b71177c0b7f608
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 14:06:26 -0400

    Update app.py

--- multipage1/app.py
+++ multipage1/app.py
@@ -8,24 +8,24 @@ home = st.Page(
 id_now = st.Page("ID_Now.py", title="ID Now", icon=":material/arrow_forward:", default=False)
 
 login = st.Page(
-    "multipage1/diagnose/Login.py", title="Login", icon=":material/login:", default=False)
+    "diagnose/Login.py", title="Login", icon=":material/login:", default=False)
 
 single = st.Page(
-    "multipage1/diagnose/Single.py", title="Single", icon=":material/file_open:", default=False)
+    "diagnose/Single.py", title="Single", icon=":material/file_open:", default=False)
 
 batch = st.Page(
-    "multipage1/diagnose/Batch.py", title = "Batch", icon=":material/stacks:", default=False) 
+    "diagnose/Batch.py", title = "Batch", icon=":material/stacks:", default=False) 
 
 tutorial = st.Page(
-    "multipage1/help/Tutorial.py", title="Tutorial", icon=":material/text_snippet:",  default=False
+    "help/Tutorial.py", title="Tutorial", icon=":material/text_snippet:",  default=False
 )
 
 faq = st.Page(
-    "multipage1/help/FAQ.py", title="FAQ", icon=":material/help:", default=False
+    "help/FAQ.py", title="FAQ", icon=":material/help:", default=False
 )
 
 contact = st.Page(
-    "multipage1/help/Contact.py", title="Contact", icon=":material/info:", default=False
+    "help/Contact.py", title="Contact", icon=":material/info:", default=False
 )
 
 
