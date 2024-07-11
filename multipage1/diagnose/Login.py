1e13c49db7359b8189330618151025355c8666c1
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-11 09:03:56 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -9,22 +9,28 @@ st.caption('''I have created accounts manually for this example.
 I would need to connect the signup page to a database in order to 
 securely store and update user information.''') 
 
-def signup(): {
-  st.form(key='login', clear_on_submit = True) 
-  st.subheader('Login')
-  username = st.text_input('Username', placeholder='Enter your username')
-  name = st.text_input('Password', placeholder='Enter your password')
+usernames = st.secrets.un
+passwords = st.secrets.pw
 
-}
+def sign_in():
+  with st.form(key='login', clear_on_submit = True) 
+    st.subheader('Login')
+    username = st.text_input('Username', placeholder='Enter your username')
+    name = st.text_input('Password', placeholder='Enter your password')
 
-  
+    if username in usernames and password in passwords:
+      st.write("Login successful")
+    else: 
+      st.warning("Username/password invalid")
+      
+sign_in()
 
 
 #print username list (un)
 for x in range(len(st.secrets.un)):
   st.write(st.secrets.un[x])
 
-st.write(st.secrets.b["username"])
-st.write(st.secrets.b["name"])
+st.write(st.secrets.bw["username"])
+st.write(st.secrets.bw["name"])
 
 
