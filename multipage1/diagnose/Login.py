25eafe00a964f8e5954c418b4de4e7b2e7c3571c
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-12 19:48:31 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -30,7 +30,7 @@ if st.session_state["authentication_status"]:
     authenticator.logout()
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Some content')
-    st.switch_page('diagnose/Single.py') #switch to sample submission page
+    #st.switch_page('diagnose/Single.py') #switch to sample submission page
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
