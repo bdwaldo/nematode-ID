cc5fa6ccda547a84523b4ac67263a494ef1b6393
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-12 13:36:02 -0400

    Update home.py

--- mulitipage2/home.py
+++ mulitipage2/home.py
@@ -1,24 +1,127 @@
-#https://docs.streamlit.io/get-started/tutorials/create-a-multipage-app
+#streamlit authenticator login
+#https://github.com/mkhorasani/Streamlit-Authenticator/tree/main?tab=readme-ov-file#authenticatelogin
 
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
 
-st.set_page_config(
-    page_title="Hello"
-   
+# Load your configuration file (replace '../config.yaml' with your actual path)
+#make sure yaml file extension is correct. won't work if created in jupytr notebook
+with open('multipage2/config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.SafeLoader)
+
+# Create an authentication object
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
 )
 
-st.write("# Welcome to Nematode ID!")
 
-st.sidebar.success("Select a Page.")
+#this function is the up to date version
+authenticator.login()
 
-st.markdown(
-    """
-    Nematode ID is a source for identifying plant-parasitic nematodes from images.
-    **Select a page from the sidebar** to get started.
-    ### Want to learn more?
-    - Check out the [Tutorial](https://streamlit.io)
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
 
-    ### Basic Information
-    - See the [Help](https://github.com/streamlit/demo-self-driving) page
-"""
-)
+
+#print user information
+#st.write(config['credentials']['usernames'])
+
+
+
+
+
+
+
+#reset password
+if st.session_state["authentication_status"]:
+    try:
+        if authenticator.reset_password(st.session_state["username"]):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
+
+
+#New user
+try:
+    email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
+    if email_of_registered_user:
+        st.success('User registered successfully')
+except Exception as e:
+    st.error(e)
+
+#forgot password
+try:
+    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
+    if username_of_forgotten_password:
+        st.success('New password to be sent securely')
+        # The developer should securely transfer the new password to the user.
+    elif username_of_forgotten_password == False:
+        st.error('Username not found')
+except Exception as e:
+    st.error(e)
+
+
+#forgot username
+try:
+    username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
+    if username_of_forgotten_username:
+        st.success('Username to be sent securely')
+        # The developer should securely transfer the username to the user.
+    elif username_of_forgotten_username == False:
+        st.error('Email not found')
+except Exception as e:
+    st.error(e)
+    
+
+#####################
+#update user information
+if st.session_state["authentication_status"]:
+    try:
+        if authenticator.update_user_details(st.session_state["username"]):
+            st.success('Entries updated successfully')
+    except Exception as e:
+        st.error(e)
+
+#st.write(config['credentials']['usernames']['jsmith'])
+
+
+
+#update user information
+#usernames = config['credentials']['usernames']
+#st.write = usernames
+
+
+#Trying to acutally update name
+#with st.form('edit_profile'):
+#    st.write('Edit Name')
+#    new_name = st.text_input('New name')
+#    submitted = st.form_submit_button('Submit Form')
+#    if (config['cookie']['name'] == new_name):
+#        st.warning('Name already in use.')
+#    else:
+#        config['cookie']['name'] = new_name
+
+
+#update config file
+try:
+    with open("multipage2/config.yaml", "w") as f:
+        yaml.dump(config, stream=f, default_flow_style=False, sort_keys=False)
+except IOError as e:
+    print(f"Error writing to file: {e}")
+
+import os
+st.write(os.getcwd())
+
+#update config file
+#with open('multipage1/diagnose/config.yaml', 'w') as file:
+#    yaml.dump(config, file, default_flow_style=False)
