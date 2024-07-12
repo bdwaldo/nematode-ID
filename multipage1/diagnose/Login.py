5259b8488ada3259bb28d7467de035fe0893db35
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-12 14:47:00 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -30,7 +30,7 @@ if st.session_state["authentication_status"]:
     authenticator.logout()
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Some content')
-    st.switch_page('multipage1/diagnose/Single.py')
+    st.switch_page('diagnose/Single.py')
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
