58488106a8696fd52c5b1f75cebc9c51fb135376
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-12 11:37:44 -0400

    Update Login.py

--- multipage1/diagnose/Login.py
+++ multipage1/diagnose/Login.py
@@ -83,7 +83,7 @@ if st.session_state["authentication_status"]:
     except Exception as e:
         st.error(e)
 
-st.write(config['usernames'])
+st.write(config['cookie']['name'])
 
 
 #update user information
