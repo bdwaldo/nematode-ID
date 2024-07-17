0b9482a878005a809cc212ab8fc2e4005ae05bd7
bdwaldo <130095936+bdwaldo@users.noreply.github.com>
2024-07-17 07:49:37 -0400

    Update ID-page.py

--- ID-page.py
+++ ID-page.py
@@ -81,7 +81,7 @@ if upload is not None:
 
   #make prediction from uploaded image
   predictions = model.predict(img_array)
-  pred_name = class_names[np.argmax(predictions, axis = 0)]
+  pred_name = class_names[np.argmax(predictions)]
   score = tf.nn.softmax(predictions[0])
 
   #print image can omit these three lines
